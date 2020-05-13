351ff60c22be0caf80b1f5869c9e111f760797da
Sherif Ashraf <sherifashraf910@gmail.com>
2020-05-13 14:00:22 +0200

    Simple Demo For Nearest Location

---
+++ GoogleMapsCustomMarkers/CustomMarkerView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomMarkerView.swift
+//  GoogleMapsCustomMarkers
+//
+//  Created by Sherif on 5/10/20.
+//  Copyright © 2020 Sherif. All rights reserved.
+//
+
+import UIKit
+
+class CustomMarkerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
